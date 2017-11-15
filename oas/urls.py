b84ea200ee0c8a0877e9f5a1238e1236f12f2b73
JerahChua <32487369+JerahChua@users.noreply.github.com>
2017-11-15 22:20:07 +0800

    Add files via upload
    Added view/approve/reject payments.

--- oas/urls.py
+++ oas/urls.py
@@ -7,5 +7,16 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^applicants/$', views.view_applicants, name='view_applicants'),
     url(r'^applicants/(?P<idnum>[0-9]{8})/$', views.view_applicant_detail, name='view_applicant_detail'),
-    url(r'^applicants/delete_(?P<idnum>[0-9]{8})/$', views.delete_student, name='delete_student')
+    url(r'^applicants/(?P<idnum>[0-9]{8})/delete/$', views.delete_student, name='delete_student'),
+    url(r'^applicants/(?P<idnum>[0-9]{8})/approve/$', views.approve_loan, name='approve_loan'),
+    url(r'^applicants/(?P<idnum>[0-9]{8})/reject/$', views.reject_loan, name='reject_loan'),
+    url(r'^loaners/$', views.view_loaners, name='view_loaners'),
+    url(r'^loaners/(?P<idnum>[0-9]{8})/$', views.view_loaner_detail, name='view_loaner_detail'),
+    url(r'^payments/pending/$', views.view_payments_pending, name='pending_payments'),
+    url(r'^payments/all/$', views.view_payments_all, name='all_payments'),
+    url(r'^payments/(?P<ornum>[\w{}.-]{1,20})/$', views.view_payment_detail, name='view_payment_detail'),
+    url(r'^payments/(?P<ornum>[\w{}.-]{1,20})/approve$', views.approve_payment, name='approve_payment'),
+    url(r'^payments/(?P<ornum>[\w{}.-]{1,20})/reject$', views.reject_payment, name='reject_payment'),
+    url(r'^payments/(?P<ornum>[\w{}.-]{1,20})/edit$', views.edit_payment, name='edit_payment'),
+
 ]
--- oas/views.py
+++ oas/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from dbhandler.models import Student, Loan, Payment
+from django.views import generic
 
 
 def index(request):
@@ -8,16 +9,99 @@ def index(request):
 
 def view_applicants(request):
     applicant_list = Student.objects.filter(loan__status="In Process")
+
     return render(request, 'applicants.html', {'applicant_list': applicant_list})
     # to pass variables into the template, define a context (with file type dict)
     # the first parameter is the name, second is the value/variable
 
 
+# class ApplicantView(generic.ListView):
+#     template_name = 'applicants.html'
+#     context_object_name = 'applicant_list'
+#
+#     def get_queryset(self):
+#         return Student.objects.filter(loan__status="In Process")
+
+
+# class ApplicantDetail(generic.DetailView):
+#     model = Student
+#     template_name = 'app_details.html'
+
+
 def view_applicant_detail(request, idnum):
     student = Student.objects.get(idNum=idnum)
-    return render(request, 'details.html', {'student': student})
+    loan = Loan.objects.get(idNum=student)
+    return render(request, 'app_details.html', {'student': student, 'loan': loan})
 
 
 def delete_student(request, idnum):
     Student.objects.get(pk=idnum).delete()
-    view_applicants(request)
+    return redirect('oas:view_applicants')
+
+
+def approve_loan(request, idnum):
+    student = Student.objects.get(idNum=idnum)
+    loan = Loan.objects.get(idNum=student)
+    loan.status = "Approved"
+    loan.balance = loan.amount
+    loan.save()
+    return render(request, 'app_details.html', {'student': student, 'loan': loan})
+
+
+def reject_loan(request, idnum):
+    student = Student.objects.get(idNum=idnum)
+    loan = Loan.objects.get(idNum=student)
+    loan.status = "Rejected"
+    loan.save()
+    return render(request, 'app_details.html', {'student': student, 'loan': loan})
+
+
+def view_loaners(request):
+    loaners = Student.objects.filter(loan__status="Approved")
+    return render(request, 'loaners.html', {'loaners': loaners})
+
+
+def view_loaner_detail(request, idnum):
+    student = Student.objects.get(idNum=idnum)
+    loan = Loan.objects.get(idNum=student)
+    return render(request, 'loaner_details.html', {'student': student, 'loan': loan})
+
+
+def view_payments_pending(request):
+    payments = Payment.objects.filter(isApproved=False)
+    show_all = False
+    return render(request, 'payments.html', {'payments': payments, 'show_all': show_all})
+
+
+def view_payments_all(request):
+    payments = Payment.objects.all()
+    show_all = True
+    return render(request, 'payments.html', {'payments': payments, 'show_all': show_all})
+
+
+def view_payment_detail(request, ornum):
+    payment = Payment.objects.get(orNum=ornum)
+    student = Student.objects.get(idNum=payment.idNum.idNum)
+    loan = Loan.objects.get(idNum=student)
+    return render(request, 'payment_details.html', {'student': student, 'payment': payment, 'loan': loan})
+
+
+def approve_payment(request, ornum):
+    payment = Payment.objects.get(orNum=ornum)
+    payment.isApproved = True
+    payment.save()
+    student = Student.objects.get(idNum=payment.idNum.idNum)
+    loan = Loan.objects.get(idNum=student)
+    loan.balance -= payment.amount
+    loan.save()
+    return redirect('oas:view_payment_detail', ornum=ornum)
+
+
+def reject_payment(request, ornum):
+    Payment.objects.get(orNum=ornum).delete()
+    return redirect('oas:all_payments')
+
+
+def edit_payment(request, ornum):
+    Payment.objects.get(orNum=ornum).delete()
+    return redirect('oas:all_payments')
